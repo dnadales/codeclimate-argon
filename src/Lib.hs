c20cb4dc1a8351a53023ff5f18f7054a7d590722
capitanbatata <damian.nadales@gmail.com>
2018-03-02 16:19:48 +0100

    A version of the main program that outputs some dummy JSON that reports a fake issue.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,4 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ "{  \"type\": \"issue\",  \"check_name\": \"Argon/Metrics/CyclomaticComplexity\",  \"description\": \"Cyclomatic complexity for complex_method is too high (10/3)\",  \"content\": \"Put some **nice** `explanation` of the -issue- in markdown\",  \"categories\": [\"Complexity\"],  \"location\": {    \"path\" :\"src/Lib.hs\",    \"begin\": {      \"line\": 3,      \"column\": 10    },    \"begin\": {      \"line\": 3,      \"column\": 20    }  },  \"other_locations\": [],  \"remediation_points\": 50000,  \"severity\": \"major\",  \"fingerprint\": \"abcd1234\"}" ++ "\0"
